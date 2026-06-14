235d48036617d5ec9cac715c47d192a179641949
idatasubcom-oss <idatasub.com@gmail.com>
2026-06-14 01:29:07 +0100

    Set up Express server with CORS and JSON support

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("I Z DATA SUB Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
